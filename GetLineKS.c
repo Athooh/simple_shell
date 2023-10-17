bed99ab3c9c891917587308635abc8726664665c
Seth Athooh <113475699+Athooh@users.noreply.github.com>
2023-10-17 19:16:37 +0300

    Update GetLineKS.c

--- GetLineKS.c
+++ GetLineKS.c
@@ -10,7 +10,7 @@
 
 ssize_t input_buf(info_t *info, char **buf, size_t *len)
 {
-	ssize_t rs = 0;
+	ssize_t r = 0;
 	size_t len_p = 0;
 
 	if (!*len)
@@ -19,27 +19,27 @@ ssize_t input_buf(info_t *info, char **buf, size_t *len)
 		*buf = NULL;
 		signal(SIGINT, sigintHandler);
 #if USE_GETLINE
-		rs = getline(buf, &len_p, stdin);
+		r = getline(buf, &len_p, stdin);
 #else
-		rs = _getline(info, buf, &len_p);
+		r = _getline(info, buf, &len_p);
 #endif
-		if (rs > 0)
+		if (r > 0)
 		{
-			if ((*buf)[rs - 1] == '\n')
+			if ((*buf)[r - 1] == '\n')
 			{
-				(*buf)[rs - 1] = '\0';
-				rs--;
+				(*buf)[r - 1] = '\0';
+				r--;
 			}
 			info->count_current_line = 1;
 			remove_comments(*buf);
 			build_hist_list(info, *buf, info->hist_line_count++);
 			{
-				*len = rs;
+				*len = r;
 				info->command_buffer = buf;
 			}
 		}
 	}
-	return (rs);
+	return (r);
 }
 
 /**
@@ -51,60 +51,60 @@ ssize_t input_buf(info_t *info, char **buf, size_t *len)
 ssize_t get_input(info_t *info)
 {
 	static char *buf;
-	static size_t xs, js, len;
-	ssize_t rs = 0;
-	char **buf_p = &(info->input_string), *ps;
+	static size_t x, j, len;
+	ssize_t r = 0;
+	char **buf_p = &(info->input_string), *p;
 
 	_putchar(BUF_FLUSH);
-	rs = input_buf(info, &buf, &len);
-	if (rs == -1)
+	r = input_buf(info, &buf, &len);
+	if (r == -1)
 		return (-1);
 	if (len)
 	{
-		js = xs;
-		ps = buf + xs;
+		j = x;
+		p = buf + x;
 
-		check_chain(info, buf, &js, xs, len);
-		while (js < len)
+		check_chain(info, buf, &j, x, len);
+		while (j < len)
 		{
-			if (is_chain(info, buf, &js))
+			if (is_chain(info, buf, &j))
 				break;
-			js++;
+			j++;
 		}
 
-		xs = js + 1;
-		if (xs >= len)
+		x = j + 1;
+		if (x >= len)
 		{
-			xs = len = 0;
+			x = len = 0;
 			info->command_buffer_type = CMD_NORM;
 		}
 
-		*buf_p = ps;
-		return (str_len(ps));
+		*buf_p = p;
+		return (str_len(p));
 	}
 
 	*buf_p = buf;
-	return (rs);
+	return (r);
 }
 
 /**
  * read_buf - it reads a buffer
  * @info: The structure containing potential arguments.
  * @buf: the buffer
- * @xs: the size
+ * @x: the size
  * Return: it returns rs
  */
 
-ssize_t read_buf(info_t *info, char *buf, size_t *xs)
+ssize_t read_buf(info_t *info, char *buf, size_t *x)
 {
-	ssize_t rs = 0;
+	ssize_t r = 0;
 
-	if (*xs)
+	if (*x)
 		return (0);
-	rs = read(info->read_file_descriptor, buf, READ_BUF_SIZE);
-	if (rs >= 0)
-		*xs = rs;
-	return (rs);
+	r = read(info->read_file_descriptor, buf, READ_BUF_SIZE);
+	if (r >= 0)
+		*x = r;
+	return (r);
 }
 
 /**
@@ -118,40 +118,40 @@ ssize_t read_buf(info_t *info, char *buf, size_t *xs)
 int _getline(info_t *info, char **ptr, size_t *length)
 {
 	static char buf[READ_BUF_SIZE];
-	static size_t is, len;
-	size_t xs;
-	ssize_t rs = 0, ss = 0;
-	char *qs = NULL, *new_p = NULL, *cs;
+	static size_t i, len;
+	size_t x;
+	ssize_t r = 0, s = 0;
+	char *q = NULL, *new_p = NULL, *c;
 
-	qs = *ptr;
-	if (qs && length)
-		ss = *length;
-	if (is == len)
-		is = len = 0;
+	q = *ptr;
+	if (q && length)
+		s = *length;
+	if (i == len)
+		i = len = 0;
 
-	rs = read_buf(info, buf, &len);
-	if (rs == -1 || (rs == 0 && len == 0))
+	r = read_buf(info, buf, &len);
+	if (r == -1 || (r == 0 && len == 0))
 		return (-1);
 
-	cs = str_chr(buf + is, '\n');
-	xs = cs ? 1 + (unsigned int)(cs - buf) : len;
-	new_p = mem_realloc(qs, ss, ss ? ss + xs : xs + 1);
+	c = str_chr(buf + i, '\n');
+	x = c ? 1 + (unsigned int)(c - buf) : len;
+	new_p = mem_realloc(q, s, s ? s + x : x + 1);
 	if (!new_p)
-		return (qs ? free(qs), -1 : -1);
+		return (q ? free(q), -1 : -1);
 
-	if (ss)
-		str_n_cat(new_p, buf + is, xs - is);
+	if (s)
+		str_n_cat(new_p, buf + i, x - i);
 	else
-		str_n_cpy(new_p, buf + is, xs - is + 1);
+		str_n_cpy(new_p, buf + i, x - i + 1);
 
-	ss += xs - is;
-	is = xs;
-	qs = new_p;
+	s += x - i;
+	i = x;
+	q = new_p;
 
 	if (length)
-		*length = ss;
-	*ptr = qs;
-	return (ss);
+		*length = s;
+	*ptr = q;
+	return (s);
 }
 
 /**
