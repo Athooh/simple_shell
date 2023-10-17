54955e6a0733f443db376e4d07fd79a82f3c8fae
Kaymoregirl <135054066+Kaymoregirl@users.noreply.github.com>
2023-10-17 20:20:59 +0300

    Update ShellVarsKS.c

--- ShellVarsKS.c
+++ ShellVarsKS.c
@@ -4,34 +4,34 @@
  * is_chain - it test if current buffer char is a chain delimeter
  * @info: The structure containing potential arguments.
  * @buf: buffer char
- * @cpstn: current position address in buf
+ * @pstn: current position address in buf
  * Return: 1 if chain delimeter, 0 otherwise
  */
 
-int is_chain(info_t *info, char *buf, size_t *cpstn)
+int is_chain(info_t *info, char *buf, size_t *pstn)
 {
-	size_t pk = *cpstn;
+	size_t p = *pstn;
 
-	if (buf[pk] == '|' && buf[pk + 1] == '|')
+	if (buf[p] == '|' && buf[p + 1] == '|')
 	{
-		buf[pk] = 0;
-		pk++;
+		buf[p] = 0;
+		p++;
 		info->command_buffer_type = CMD_OR;
 	}
-	else if (buf[pk] == '&' && buf[pk + 1] == '&')
+	else if (buf[p] == '&' && buf[p + 1] == '&')
 	{
-		buf[pk] = 0;
-		pk++;
+		buf[p] = 0;
+		p++;
 		info->command_buffer_type = CMD_AND;
 	}
-	else if (buf[pk] == ';')
+	else if (buf[p] == ';')
 	{
-		buf[pk] = 0;
+		buf[p] = 0;
 		info->command_buffer_type = CMD_CHAIN;
 	}
 	else
 		return (0);
-	*cpstn = pk;
+	*pstn = p;
 	return (1);
 }
 
@@ -47,14 +47,14 @@ int is_chain(info_t *info, char *buf, size_t *cpstn)
 
 void check_chain(info_t *info, char *buf, size_t *pstn, size_t idx, size_t len)
 {
-	size_t xk = *pstn;
+	size_t x = *pstn;
 
 	if (info->command_buffer_type == CMD_AND)
 	{
 		if (info->command_status)
 		{
 			buf[idx] = 0;
-			xk = len;
+			x = len;
 		}
 	}
 	if (info->command_buffer_type == CMD_OR)
@@ -62,11 +62,11 @@ void check_chain(info_t *info, char *buf, size_t *pstn, size_t idx, size_t len)
 		if (!info->command_status)
 		{
 			buf[idx] = 0;
-			xk = len;
+			x = len;
 		}
 	}
 
-	*pstn = xk;
+	*pstn = x;
 }
 
 /**
@@ -77,23 +77,23 @@ void check_chain(info_t *info, char *buf, size_t *pstn, size_t idx, size_t len)
 
 int alias_rep(info_t *info)
 {
-	int xk;
+	int x;
 	list_t *node;
-	char *sk;
+	char *s;
 
-	for (xk = 0; xk < 10; xk++)
+	for (x = 0; x < 10; x++)
 	{
 		node = node_startswith(info->alias_list, info->input_array[0], '=');
 		if (!node)
 			return (0);
 		free(info->input_array[0]);
-		sk = str_chr(node->str, '=');
-		if (!sk)
+		s = str_chr(node->str, '=');
+		if (!s)
 			return (0);
-		sk = str_dup(sk + 1);
-		if (!sk)
+		s = str_dup(s + 1);
+		if (!s)
 			return (0);
-		info->input_array[0] = sk;
+		info->input_array[0] = s;
 	}
 	return (1);
 }
@@ -106,34 +106,34 @@ int alias_rep(info_t *info)
 
 int vars_rep(info_t *info)
 {
-	int xk = 0;
+	int x = 0;
 	list_t *node;
 
-	for (xk = 0; info->input_array[xk]; xk++)
+	for (x = 0; info->input_array[x]; x++)
 	{
-		if (info->input_array[xk][0] != '$' || !info->input_array[xk][1])
+		if (info->input_array[x][0] != '$' || !info->input_array[x][1])
 			continue;
 
-		if (!str_cmp(info->input_array[xk], "$?"))
+		if (!str_cmp(info->input_array[x], "$?"))
 		{
-			str_rep(&(info->input_array[xk]),
+			str_rep(&(info->input_array[x]),
 					str_dup(convert_num(info->command_status, 10, 0)));
 			continue;
 		}
-		if (!str_cmp(info->input_array[xk], "$$"))
+		if (!str_cmp(info->input_array[x], "$$"))
 		{
-			str_rep(&(info->input_array[xk]),
+			str_rep(&(info->input_array[x]),
 					str_dup(convert_num(getpid(), 10, 0)));
 			continue;
 		}
-		node = node_startswith(info->env_variable, &info->input_array[xk][1], '=');
+		node = node_startswith(info->env_variable, &info->input_array[x][1], '=');
 		if (node)
 		{
-			str_rep(&(info->input_array[xk]),
+			str_rep(&(info->input_array[x]),
 					str_dup(str_chr(node->str, '=') + 1));
 			continue;
 		}
-		str_rep(&info->input_array[xk], str_dup(""));
+		str_rep(&info->input_array[x], str_dup(""));
 
 	}
 	return (0);
