246e67b170cec3539d5bb25d83e874b5d23311ac
Seth Athooh <113475699+Athooh@users.noreply.github.com>
2023-10-17 20:44:22 +0300

    Update ShellErrorsKS.c

--- ShellErrorsKS.c
+++ ShellErrorsKS.c
@@ -1,142 +1,85 @@
 #include "shell.h"
 
 /**
- * _erratoi - it converts string to an integer
- * @st: the converted string
- * Return: returns 0 if no numbers in string, converted number otherwise
- *       returns -1 on error
+ * eput_str - it prints input string
+ * @str: printed string
+ * Return: returns nothing
  */
 
-int _erratoi(char *st)
+void eput_str(char *str)
 {
 	int x = 0;
-	unsigned long int result = 0;
 
-	if (*st == '+')
-		st++;
-	for (x = 0;  st[x] != '\0'; x++)
+	if (!str)
+		return;
+	while (str[x] != '\0')
 	{
-		if (st[x] >= '0' && st[x] <= '9')
-		{
-			result *= 10;
-			result += (st[x] - '0');
-			if (result > INT_MAX)
-				return (-1);
-		}
-		else
-			return (-1);
+		eput_char(str[x]);
+		x++;
 	}
-	return (result);
 }
 
 /**
- * print_error - it prints error message
- * @info: The structure containing potential arguments.
- * @err_str: the string containing specified error type
- * Return: returns 0 if no numbers in string, converted number otherwise
- *        returns -1 on error
+ * eput_char - it writes character c to stderr
+ * @ch: character to print
+ * Return: returns 1 On success.
+ * returns -1 on error, and errno is set appropriately.
  */
 
-void print_error(info_t *info, char *err_str)
+int eput_char(char ch)
 {
-	eput_str(info->program_filename);
-	eput_str(": ");
-	print_d(info->error_count, STDERR_FILENO);
-	eput_str(": ");
-	eput_str(info->input_array[0]);
-	eput_str(": ");
-	eput_str(err_str);
+	static int x;
+	static char buf[WRITE_BUF_SIZE];
+
+	if (ch == BUF_FLUSH || x >= WRITE_BUF_SIZE)
+	{
+		write(2, buf, x);
+		x = 0;
+	}
+	if (ch != BUF_FLUSH)
+		buf[x++] = ch;
+	return (1);
 }
 
 /**
- * print_d - function to print decimal (integer) number (base 10)
- * @input: the input
- * @fdr: the filedescriptor to write on
- * Return: returns number of printed characters
+ * _putfd - it writes character c to given fdr
+ * @ch: printed character
+ * @fdr: filedescriptor to write on
+ * Return: returns 1 on success.
+ * returns -1 on error, and errno is set appropriately.
  */
 
-int print_d(int input, int fdr)
+int _putfd(char ch, int fdr)
 {
-	int (*__putchar)(char) = _putchar;
-	int x, count = 0;
-	unsigned int _abs_, current;
+	static int x;
+	static char buf[WRITE_BUF_SIZE];
 
-	if (fdr == STDERR_FILENO)
-		__putchar = eput_char;
-	if (input < 0)
+	if (ch == BUF_FLUSH || x >= WRITE_BUF_SIZE)
 	{
-		_abs_ = -input;
-		__putchar('-');
-		count++;
+		write(fdr, buf, x);
+		x = 0;
 	}
-	else
-		_abs_ = input;
-	current = _abs_;
-	for (x = 1000000000; x > 1; x /= 10)
-	{
-		if (_abs_ / x)
-		{
-			__putchar('0' + current / x);
-			count++;
-		}
-		current %= x;
-	}
-	__putchar('0' + current);
-	count++;
-
-	return (count);
+	if (ch != BUF_FLUSH)
+		buf[x++] = ch;
+	return (1);
 }
 
 /**
- * convert_num - function converter, str-word clone
- * @numb: the number
- * @base: the base
- * @flags: flags argument
- * Return: returns string
+ * _putsfd - it prints string input
+ * @str: the printed string
+ * @fdr: filedescriptor to write on
+ * Return: returns the number of chars put
  */
 
-char *convert_num(long int numb, int base, int flags)
+int _putsfd(char *str, int fdr)
 {
-	static char *array;
-	static char buffer[50];
-	char sign = 0;
-	char *ptr;
-	unsigned long n = numb;
+	int x = 0;
 
-	if (!(flags & CONVERT_UNSIGNED) && numb < 0)
+	if (!str)
+		return (0);
+	while (*str)
 	{
-		n = -numb;
-		sign = '-';
-
+		x += _putfd(*str++, fdr);
 	}
-	array = flags & CONVERT_LOWERCASE ? "0123456789abcdef" : "0123456789ABCDEF";
-	ptr = &buffer[49];
-	*ptr = '\0';
-
-	do	{
-		*--ptr = array[n % base];
-		n /= base;
-	} while (n != 0);
-
-	if (sign)
-		*--ptr = sign;
-	return (ptr);
-}
-
-/**
- * remove_comments - it replaces first instance of '#' with '\0'
- * @buf: string modified address
- * Return: returns 0 always;
- */
-
-void remove_comments(char *buf)
-{
-	int x;
-
-	for (x = 0; buf[x] != '\0'; x++)
-		if (buf[x] == '#' && (!x || buf[x - 1] == ' '))
-		{
-			buf[x] = '\0';
-			break;
-		}
+	return (x);
 }
